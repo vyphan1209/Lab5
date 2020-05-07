acb09000041128d81af7d6bcab5bdbe0bd460489
Phan Thi Ngoc Vy <18dh110199@st.huflit.edu.vn>
2020-05-07 13:15:15 +0700

    Add project files.

---
+++ bai1/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace bai1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxX = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxY = new System.Windows.Forms.TextBox();
+            this.btnXuLy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "vui long nhap x:";
+            // 
+            // txtBoxX
+            // 
+            this.txtBoxX.Location = new System.Drawing.Point(100, 19);
+            this.txtBoxX.Name = "txtBoxX";
+            this.txtBoxX.Size = new System.Drawing.Size(132, 20);
+            this.txtBoxX.TabIndex = 1;
+            this.txtBoxX.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "vui long nhap y:";
+            // 
+            // txtBoxY
+            // 
+            this.txtBoxY.Location = new System.Drawing.Point(100, 55);
+            this.txtBoxY.Name = "txtBoxY";
+            this.txtBoxY.Size = new System.Drawing.Size(132, 20);
+            this.txtBoxY.TabIndex = 3;
+            this.txtBoxY.Text = "0";
+            // 
+            // btnXuLy
+            // 
+            this.btnXuLy.Location = new System.Drawing.Point(59, 191);
+            this.btnXuLy.Name = "btnXuLy";
+            this.btnXuLy.Size = new System.Drawing.Size(157, 29);
+            this.btnXuLy.TabIndex = 4;
+            this.btnXuLy.Text = "Hien thi noi dung textBox";
+            this.btnXuLy.UseVisualStyleBackColor = true;
+            this.btnXuLy.Click += new System.EventHandler(this.btnXuLy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnXuLy);
+            this.Controls.Add(this.txtBoxY);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBoxX);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Bai1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxX;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxY;
+        private System.Windows.Forms.Button btnXuLy;
+    }
+}
+
